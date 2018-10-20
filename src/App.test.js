cad14e97cd67ac002abc1d6ab3352b07f60bc5f5
Ahmed Subkhi <ahmed.subkhi@gmail.com>
2018-10-20 21:28:27 +0700

    Fix unit testing

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import App from 'App';
 
 it('renders without crashing', () => {
+  global.localStorage = {
+    getItem: function () {
+      return null
+    }
+  };
+
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
