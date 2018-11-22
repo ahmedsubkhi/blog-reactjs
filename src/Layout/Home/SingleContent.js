032abdfa81c705b340da6081a955898da624f2c5
Ahmed Subkhi <ahmed.subkhi@gmail.com>
2018-11-23 06:56:49 +0700

    -Modify admin layout -Add comment container

--- src/Layout/Home/SingleContent.js
+++ src/Layout/Home/SingleContent.js
@@ -47,13 +47,38 @@ class SingleContent extends Component {
     return body;
   }
 
+  renderComments() {
+    const comments = this.state.posts.comments;
+
+    return comments.map((comment, i) =>
+      <div className="container-comment-post" key={i}>
+        <div className="container">
+          <small className="user-comment-post text-danger">Commented by: {comment.id_user} <br /> At: {this.dateHuman(comment.created_at)}</small>
+          <div className="body-comment-post">
+            {comment.body}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  dateHuman(strDate){
+    var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    var date = new Date(strDate);
+    return date.toLocaleString('en-GB', options);
+  }
+
   render(){
     if(this.state.ready){
       const postsData = this.state.posts;
+      const renderComments = this.renderComments();
 
       return (
         <div className="content-blog">
           <Post posts={postsData} />
+          <div className="comments">
+            {renderComments}
+          </div>
         </div>
       );
     } else {
